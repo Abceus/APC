4aecd0e90131e4e117d7611453222aadafb98578
Jamil Guseynov <j.guseynov@octaviangaming.com>
2019-02-27 09:51:45 +0300

    update zoom

--- android/app/src/main/java/com/apc/testapplication/MainActivity.java
+++ android/app/src/main/java/com/apc/testapplication/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends Activity implements OnTouchListener {
         mView = new GL2JNIView(getApplication());
         mView.setOnTouchListener(this);
         setContentView(mView);
-        pointerIds[0] = -1;
-        pointerIds[1] = -1;
     }
 
     @Override protected void onPause() {
@@ -37,7 +35,6 @@ public class MainActivity extends Activity implements OnTouchListener {
  
 	private Timer holdTimer;
     private HoldTouchTask holdTimerTask;
-    private int[] pointerIds = new int[2];
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -45,71 +42,37 @@ public class MainActivity extends Activity implements OnTouchListener {
         int action = event.getActionMasked();
         int pointerId = event.getPointerId(index);
 
-        Log.v("APC", "Even. Type: " + event.getAction() + ", index: " + index + ", action: " + action + ", pointer: " + pointerId);
-
         int x = (int)MotionEventCompat.getX(event, index);
         int y = (int)MotionEventCompat.getY(event, index);
 
-        // TODO: rework multitouch
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                pointerIds[0] = pointerId;
                 holdTimer = new Timer();
                 holdTimerTask = new HoldTouchTask(x, y);
                 holdTimer.schedule(holdTimerTask, 2000);
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
-                if(pointerIds[1] == -1)
-                {
-                    pointerIds[1] = pointerId;
-                    holdTimer.cancel();
-                }
+                holdTimer.cancel();
                 break;
             case MotionEvent.ACTION_MOVE:
-                if( pointerIds[0] != -1 )
+                if( event.getPointerCount() > 1 )
                 {
-                    if( pointerIds[1] != -1 )
+                    if( pointerId <= 1 )
                     {
-                        if( pointerId == pointerIds[0] || pointerId == pointerIds[1] )
-                        {
-                            // calc dist, zoom
-                            APCLib.zoom(1.f);
-                        }
+                        // calc dist, zoom
+                        APCLib.zoom(1.f);
                     }
-                    else if( pointerId == pointerIds[0] )
-                    {
-                        holdTimer.cancel();
-                        APCLib.holdedMove(x, y);
-                    }
+                }
+                else
+                {
+                    holdTimer.cancel();
+                    APCLib.holdedMove(x, y);
                 }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                if( pointerId == pointerIds[0])
-                {
-                    if( pointerIds[1] == -1)
-                    {
-                        pointerIds[0] = -1;
-                        holdTimer.cancel();
-                        APCLib.button(x, y);
-                    }
-                    // else
-                    // {
-                    //     pointerIds[0] = pointerIds[1];
-                    //     pointerIds[1] = -1;
-                    // }
-                } 
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                if( pointerId == pointerIds[1] )
-                {
-                    pointerIds[1] = -1;
-                } 
-                else if( pointerId == pointerIds[0] )
-                {
-                    pointerIds[0] = pointerIds[1];
-                    pointerIds[1] = -1;
-                }
+                holdTimer.cancel();
+                APCLib.button(x, y);
                 break;
         }
         return true;
@@ -125,11 +88,7 @@ public class MainActivity extends Activity implements OnTouchListener {
         }
 		@Override
 		public void run() {
-            if( pointerIds[1] == -1 )
-            {
-                pointerIds[0] = -1;
-                APCLib.altButton(x, y);
-            }
+            APCLib.altButton(x, y);
 		}
 	}
 }
